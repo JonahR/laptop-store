1a2ae3c40299aa3ad941438022ab073ff63f7163
Jonah Rueb <ruebjonah@gmail.com>
2019-07-19 19:03:46 -0500

    Image routing

--- index.js
+++ index.js
@@ -9,11 +9,22 @@ const server = http.createServer((req, res) => {
     
     const pathName = url.parse(req.url, true).pathname;
     const id = url.parse(req.url, true).query.id;
-    
+
+
 
     if(pathName === '/products' || pathName === '/starter') {
         res.writeHead(200, {'Content-type': 'text/html'});
 
+        fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
+            let overviewOutput = data;
+
+            fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8', (err, data) => {
+                const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');                
+                overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput);
+
+                res.end(overviewOutput);
+            });
+        });
 
     } else if (pathName === '/laptop' && id < laptopData.length) {
         res.writeHead(200, {'Content-type': 'text/html'});
@@ -22,8 +33,13 @@ const server = http.createServer((req, res) => {
             const laptop = laptopData[id];
             const output = replaceTemplate(data, laptop); 
             res.end(output);
+        }); 
+    } else if ((/\.(jpg|jpeg|png|gif)$/i).test(pathName)) {
+        fs.readFile(`${__dirname}/data/img${pathName}`, (err, data) => {
+            res.writeHead(200, {'Content-type': 'image/jpg'});
+            res.end(data);
         });
-    } else {
+    }else {
         res.writeHead(404, {'Content-type': 'text/html'});
         res.end('URL not found.');
     }
